d3dd59b98c4d0ed76b91dd463fb0179880ccfa9c
Felix Kunzmann <fe.kunzmann@gmail.com>
2015-09-28 18:27:04 +0200

    More square merging code.

--- polygons.py
+++ polygons.py
@@ -1,37 +1,62 @@
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 
 Point = namedtuple('Point', ['x', 'y'])
+Edge = namedtuple('Edge', ['start', 'vertical'])
 
 class Square(namedtuple('Square', ['position', 'value'])):
     def __init__(self, position, value):
         x, y = position
-        self.coordinates = [Point(x, y), Point(x, y + 1), Point(x + 1, y + 1), Point(x + 1, y)]
+        self.vertices = [Point(x, y), Point(x, y + 1), Point(x + 1, y + 1), Point(x + 1, y)]
 
-    def sides(self):
-        yield self.coordinates[0], self.coordinates[1]
-        yield self.coordinates[1], self.coordinates[2]
-        yield self.coordinates[2], self.coordinates[3]
-        yield self.coordinates[3], self.coordinates[0]
+    def edges(self):
+        yield Edge(self.vertices[ 0], vertical = True)
+        yield Edge(self.vertices[+1], vertical = False)
+        yield Edge(self.vertices[-1], vertical = True)
+        yield Edge(self.vertices[ 0], vertical = False)
+
+    def as_polygon(self):
+        return Polygon(self.vertices, self.value)
 
 class Polygon(object):
-    def __init__(self, coordinates = [], value = None):
-        self.coordinates = coordinates
+    def __init__(self, vertices = [], value = None):
+        self.vertices = vertices
         self.value = value
 
+    def edge_vertex_points(self, number):
+        start = 1 - len(self.vertices)
+        return self.vertices[number], self.vertices[start + number]
+
 def create_polygones(squares):
-    polygons = []
-    sides = {}
+    polygons = {}
+    edges = {}
     count = 0
 
     class Index(object):
-        def __init__(self, polygon_number, coordinate_number):
+        def __init__(self, polygon_number, edge_number):
             self.polygon_number = polygon_number
-            self.side_number = side_number
+            self.edge_number = edge_number
 
     for square in squares:
-        for side in square.sides():
-            if side in sides:
-                index = sides[side]
+        neighbours = defaultdict(list)
+        free_edges = {}
 
+        for number, edge in enumerate(square.edges()):
+            if edge in edges:
+                index = edges[edge]
+                polygon = polygons[index.polygon_number]
+                if square.value == polygon.value:
+                    neighbours[polygon].append((index.edge_number, edge, number))
+            free_edges[number] = edge
 
-                
+        if neighbours:
+            pass
+
+        else:
+            polygon_number = count
+            polygons[count] = square.as_polygon()
+            count += 1
+
+        for number, edge in free_edges.items():
+            edges[edge] = Index(polygon_number, number)
+
+    return polygons.values()
